4fdbea13707ed7c52508223444487da9183b414a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-18 16:43:57 -0600

    it demonstrates how to use arrays and keys in react

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let myATags = links.map((link) => <a href={"#" + link} key={link}>{link}</a>);
+
+  return <nav>
+    {myATags}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let mytecharr = null;
+  if (technologies === undefined || technologies === null);
+  else
+  {
+    mytecharr = technologies.map((tech) => <span key={tech}>{tech}</span>);
+  }
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {mytecharr}
       </div>
     </div>
   );
